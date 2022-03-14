96bb1fc85c3dded1b046cafd86ba3edafbf48e27
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-03-15 08:15:46 +0900

    created config for fluentbit

---
+++ fluentbit/mod_record.lua
@@ -0,0 +1,9 @@
+function add_logged_at(tag,timestamp,record)
+    a,b = math.modf(timestamp)
+    record["inserted_at"] = string.format(
+        "%s.%06dZ",
+        os.date("!%FT%T", a),
+        math.floor(b * 1000 * 1000)
+    )
+    return 1, timestamp, record
+end
