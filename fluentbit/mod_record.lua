9a44420c60395f49757ddf1adcb178a0dab060fc
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-03-15 09:07:47 +0900

    created custom lua scripts

--- fluentbit/mod_record.lua
+++ fluentbit/mod_record.lua
@@ -2,7 +2,7 @@ function add_logged_at(tag,timestamp,record)
     a,b = math.modf(timestamp)
     record["inserted_at"] = string.format(
         "%s.%06dZ",
-        os.date("!%FT%T", a),
+        os.date("%FT%T", a),
         math.floor(b * 1000 * 1000)
     )
     return 1, timestamp, record
